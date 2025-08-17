83022f407507987021f66d65afd04c0ec5efee0c
hema0987654 <142291949+hema0987654@users.noreply.github.com>
2025-08-17 16:58:56 +0300

    jhjfdfdf

--- dist/main.js
+++ dist/main.js
@@ -1,8 +1,14 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
+const serverless_express_1 = __importDefault(require("@vendia/serverless-express"));
 const swagger_1 = require("@nestjs/swagger");
+let server;
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = new swagger_1.DocumentBuilder()
@@ -13,8 +19,13 @@ async function bootstrap() {
         .build();
     const document = swagger_1.SwaggerModule.createDocument(app, config);
     swagger_1.SwaggerModule.setup('api-docs', app, document);
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    server = (0, serverless_express_1.default)({ app: expressApp });
 }
 bootstrap();
+const handler = (event, context, callback) => {
+    return server(event, context, callback);
+};
+exports.handler = handler;
 //# sourceMappingURL=main.js.map
