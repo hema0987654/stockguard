75c99e51c518c71d973c86eb5af6a11f7b14dc3f
hema0987654 <142291949+hema0987654@users.noreply.github.com>
2025-08-17 16:51:44 +0300

    jh

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import serverlessExpress from '@vendia/serverless-express';
+import { Handler } from 'aws-lambda';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+let server: Handler;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // إعداد Swagger
   const config = new DocumentBuilder()
     .setTitle('Stock Guard API')
     .setDescription('API documentation for Stock Guard project')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
 
-  const port = process.env.PORT || 3000; 
-  await app.listen(port);
+  await app.init();
+  const expressApp = app.getHttpAdapter().getInstance();
+  server = serverlessExpress({ app: expressApp });
 }
+
 bootstrap();
+
+export const handler: Handler = async (event, context) => {
+  return server(event, context);
+};
