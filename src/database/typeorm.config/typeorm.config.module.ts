7d80dc4488a1cbac5079c39c57d2cf4e53e6cec2
hema0987654 <142291949+hema0987654@users.noreply.github.com>
2025-08-17 04:20:10 +0300

    Update configuration and main app files

--- src/database/typeorm.config/typeorm.config.module.ts
+++ src/database/typeorm.config/typeorm.config.module.ts
@@ -7,13 +7,10 @@ import { ConfigModule } from '@nestjs/config';
     ConfigModule.forRoot({ isGlobal: true }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432', 10),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_NAME,
+      url: process.env.DATABASE_URL,
       autoLoadEntities: true,
-      synchronize: true,
+      synchronize: true, 
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     }),
   ],
 })
